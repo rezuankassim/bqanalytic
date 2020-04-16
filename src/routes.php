89c6b051102ac1c1c9eea54cf4061cda459726bf
Rezuan Kassim <rezuankassim@hotmail.com>
2020-04-16 19:08:59 +0800

    add prebuilt page for analytics

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'namespace' => 'RezuanKassim\BQAnalytic\Http\Controllers',
-    'prefix' => 'bqanalytics'
+    'prefix' => 'bqanalytics',
+    'middleware' => 'web',
 ], function () {
-    Route::get('/', 'BQAnalyticController@index');
+    Route::get('/', 'BQAnalyticController@index')->name('bqanalytics.index');
+    Route::post('/analytics', 'BQAnalyticController@analytic')->name('bqanalytics.analytic');
 });
