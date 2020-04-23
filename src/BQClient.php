961cbfda2d824caa2ec8d898ce6e1e7acd5e08b3
Rezuan Kassim <rezuankassim@hotmail.com>
2020-04-23 13:17:18 +0800

    added functionality to query client from database

---
+++ src/BQClient.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RezuanKassim\BQAnalytic;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+
+class BQClient extends Model
+{
+    protected $table = 'bq_clients';
+
+    protected $guarded = ['created_at', 'updated_at', 'id'];
+
+    protected $casts = [
+        'status' => 'boolean'
+    ];
+
+    protected static function booted()
+    {
+        static::deleted(function ($client) {
+            if (File::exists(public_path('storage/'.$client->google_credential))) {
+                File::delete(public_path('storage/'.$client->google_credential));
+            }
+        });
+    }
+}
